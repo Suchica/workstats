362ab1817c4154d3001ca72ad9e86b5c240a861b
Hiroshi Nishio <4620828+hiroshinishio@users.noreply.github.com>
2022-06-12 21:58:25 +0900

    Resolved conflicts

--- components/common/Login.tsx
+++ components/common/Login.tsx
@@ -20,9 +20,10 @@ import SlackIcon from '../../public/slack-svgrepo-com.svg';
 import GoogleCalendarIcon from '../../public/google-calendar-svgrepo-com.svg';
 import GitHubIcon2 from '../../public/github-svgrepo-com.svg';
 
-// Custom services
+// Next related and Custom services
 import { createUserDoc } from '../../services/setDocToFirestore';
 import ProductHunt from '../buttons/ProductHunt';
+import Head from 'next/head';
 
 // Scopes in detail: https://developers.google.com/identity/protocols/oauth2/scopes#calendar
 const scopes = 'https://www.googleapis.com/auth/calendar.readonly	';
@@ -86,92 +87,118 @@ const Login = () => {
 
   // const d = new Date();
   // const year = d.getFullYear();
+  const contentLong =
+    'WorkStats is a dashboard tool for engineers and project managers engaged in system development to visualize their productivity and contributions in numbers. It aggregates various numbers from the platforms used by members and teams, such as GitHub for source control, Asana for task management, and Slack for communication tools.WorkStats';
+  const contentShort =
+    'is a dashboard tool for engineers and PMs to quantify their productivity, aggregating various numbers from GitHub, Asana, Slack, etc.';
 
   return (
-    <div className='h-screen md:flex'>
-      <div className='grid gap-4 md:gap-6 md:w-1/2 h-1/2 md:h-screen bg-slate-800 content-center justify-center justify-items-center'>
-        <div className='grid gap-0.5 md:gap-2 place-items-center'>
-          <h1 className='text-white text-3xl md:text-5xl text-center'>
-            Welcome to &quot;WorkStats&quot;
-          </h1>
-          <span className='text-green-300 text-base md:text-lg px-1.5 py-0 border-green-300 border rounded-md'>
-            Beta
-          </span>
+    <>
+      <Head>
+        <title>Dashboard - WorkStats</title>
+        <meta name='description' content={contentLong} />
+        <meta property='og:title' content='WorkStats' />
+        <meta property='og:description' content={contentShort} />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://workstats.dev' />
+        <meta property='og:image' content='../public/WorkStats_Dashboard.png' />
+        <meta property='og:image:alt' content='WorkStats' />
+        <meta property='og:image:type' content='image/png' />
+        <meta property='og:site_name' content='WorkStats' />
+        <meta property='og:locale' content='en_US' />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:title' content='WorkStats' />
+        <meta name='twitter:site' content='@hnishio0105' />
+        <meta name='twitter:creator' content='@hnishio0105' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/icononly_transparent_nobuffer.ico' />
+      </Head>
+
+      <div className='h-screen md:flex'>
+        <div className='grid gap-4 md:gap-6 md:w-1/2 h-1/2 md:h-screen bg-slate-800 content-center justify-center justify-items-center'>
+          <div className='grid gap-0.5 md:gap-2 place-items-center'>
+            <h1 className='text-white text-3xl md:text-5xl text-center'>
+              Welcome to &quot;WorkStats&quot;
+            </h1>
+            <span className='text-green-300 text-base md:text-lg px-1.5 py-0 border-green-300 border rounded-md'>
+              Beta
+            </span>
+          </div>
+          <p className='text-white px-4 text-lg md:text-2xl text-center'>
+            This app visualizes contribution of you and your team in numbers!
+            Aggregates from the following services.
+          </p>
+          <div className='flex gap-3'>
+            <Image
+              src={GitHubIcon2}
+              width={30}
+              height={30}
+              layout='intrinsic'
+              alt='GitHub logo'
+              quality={75}
+              priority={false}
+              placeholder='empty'
+            />
+            <Image
+              src={AsanaIcon}
+              width={30}
+              height={30}
+              layout='intrinsic'
+              alt='Asana logo'
+              quality={75}
+              priority={false}
+              placeholder='empty'
+            />
+            <Image
+              src={SlackIcon}
+              width={30}
+              height={30}
+              layout='intrinsic'
+              alt='Slack logo'
+              quality={75}
+              priority={false}
+              placeholder='empty'
+            />
+            <Image
+              src={GoogleCalendarIcon}
+              width={30}
+              height={30}
+              layout='intrinsic'
+              alt='Google Calendar logo'
+              quality={75}
+              priority={false}
+              placeholder='empty'
+            />
+          </div>
         </div>
-        <p className='text-white px-4 text-lg md:text-2xl text-center'>
-          This app visualizes contribution of you and your team in numbers!
-          Aggregates from the following services.
-        </p>
-        <div className='flex gap-3'>
-          <Image
-            src={GitHubIcon2}
-            width={30}
-            height={30}
-            layout='intrinsic'
-            alt='GitHub logo'
-            quality={75}
-            priority={false}
-            placeholder='empty'
-          />
-          <Image
-            src={AsanaIcon}
-            width={30}
-            height={30}
-            layout='intrinsic'
-            alt='Asana logo'
-            quality={75}
-            priority={false}
-            placeholder='empty'
-          />
-          <Image
-            src={SlackIcon}
-            width={30}
-            height={30}
-            layout='intrinsic'
-            alt='Slack logo'
-            quality={75}
-            priority={false}
-            placeholder='empty'
-          />
-          <Image
-            src={GoogleCalendarIcon}
-            width={30}
-            height={30}
-            layout='intrinsic'
-            alt='Google Calendar logo'
-            quality={75}
-            priority={false}
-            placeholder='empty'
-          />
+        <div className='grid gap-4 md:w-1/2 h-1/2 md:h-screen bg-white content-center justify-center justify-items-center'>
+          <p className='text-lg md:text-xl text-slate-900'>
+            Log in with OAuth provider
+          </p>
+          <button
+            className='text-white bg-blue-600 hover:bg-blue-800 font-bold rounded-md w-52 h-10'
+            onClick={loginWithGoogle}
+          >
+            <GoogleIcon className='text-white mr-2' />
+            Login with Google
+          </button>
+          <button
+            className='text-white bg-gray-600 hover:bg-gray-800 font-bold rounded-md w-52 h-10'
+            onClick={loginWithGithub}
+          >
+            <GitHubIcon className='text-white mr-2' />
+            Login with GitHub
+          </button>
+          <p className='text-lg md:text-xl text-slate-900'>
+            Upvote me on Product Hunt!
+          </p>
+          <ProductHunt />
         </div>
-      </div>
-      <div className='grid gap-4 md:w-1/2 h-1/2 md:h-screen bg-white content-center justify-center justify-items-center'>
-        <p className='text-lg md:text-xl text-slate-900'>
-          Log in with OAuth provider
-        </p>
-        <button
-          className='text-white bg-blue-600 hover:bg-blue-800 font-bold rounded-md w-52 h-10'
-          onClick={loginWithGoogle}
-        >
-          <GoogleIcon className='text-white mr-2' />
-          Login with Google
-        </button>
-        <button
-          className='text-white bg-gray-600 hover:bg-gray-800 font-bold rounded-md w-52 h-10'
-          onClick={loginWithGithub}
-        >
-          <GitHubIcon className='text-white mr-2' />
-          Login with GitHub
-        </button>
-        <p className='text-lg md:text-xl text-slate-900'>
-          Upvote me on Product Hunt!
-        </p>
-        <ProductHunt />
-      </div>
-      {/* <span className='text-sm md:text-base'>
+        {/* <span className='text-sm md:text-base'>
         Copyright {year} Suchica, Inc. All rights reserved.
-      </span> */}
-    </div>
+        </span> */}
+      </div>
+    </>
   );
 };
 
